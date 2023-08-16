5b70d297c2c8e49736f0688bf97463fd7abf22ea
andriiansyah <andsyah27@gmail.com>
2023-08-16 12:30:17 +0700

    looping all data done

--- src/controllers/controller-home.js
+++ src/controllers/controller-home.js
@@ -1,8 +1,39 @@
+const config = require('../configs/database');
+
+let mysql      = require('mysql');
+let pool       = mysql.createPool(config);
+
+pool.on('error',(err)=> {
+    console.error(err);
+});
+
+// module.exports ={
+//     home(req,res){
+//         res.render("home",{
+//             url: 'http://localhost:5050/',
+//             userName: req.session.username,
+//         });
+//     }
+// }
+
 module.exports ={
     home(req,res){
-        res.render("home",{
-            url: 'http://localhost:5050/',
-            userName: req.session.username,
-        });
+        let id = req.session.userid
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                SELECT * FROM user;
+                `
+            , function (error, results) {
+                if(error) throw error;
+                res.render("home",{
+                    url: 'http://localhost:5050/',
+                    userName: req.session.username,
+                    data: results,
+                });
+            });
+            connection.release();
+        })
     }
 }
